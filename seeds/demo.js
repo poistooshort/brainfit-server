462196c98f4aa4e139ae7bfc9bdcfffcbfd34cce
Po Lin <po.ting.lin@gmail.com>
2022-08-09 11:33:32 -0400

    Add comments seed data

--- seeds/demo.js
+++ seeds/demo.js
@@ -1,5 +1,6 @@
 const usersData = require('./seed_data/users');
 const exercisesData = require('./seed_data/exercises');
+const commentsData = require('./seed_data/comments');
 
 exports.seed = function (knex) {
 	return knex('users')
@@ -12,5 +13,11 @@ exports.seed = function (knex) {
 		})
 		.then(() => {
 			return knex('exercises').insert(exercisesData);
+		})
+		.then(() => {
+			return knex('comments').del();
+		})
+		.then(() => {
+			return knex('comments').insert(commentsData);
 		});
 };
---
+++ seeds/seed_data/comments.js
@@ -0,0 +1,56 @@
+module.exports=[
+	{
+		id: 1,
+		exerciseId: 1,
+		comment: "I hate these so much but they really work out your full body...",
+		username: 'Bonnie Smith' 
+	},
+	{
+		id: 2,
+		exerciseId: 1,
+		comment: "I definitely agree with Bonnie, these are such a pain and you will DEFINITELY feel them the next day.",
+		username: 'Bonnie Smith' 
+	},
+	{
+		id: 3,
+		exerciseId: 1, 
+		comment: "I don't know what you all are talking about. These are the best!"
+		username: 'Danny Williams' 
+	},
+	{
+		id: 4,
+		exerciseId: 2,
+		comment: "These are my #1 go to exercises.",
+		username: 'Bonnie Smith' 
+	},
+	{
+		id: 5,
+		exerciseId: 2,
+		comment: "I like these but I use the barbell version instead. I'll need to upload a gif for it later.",
+		username: 'Danny Williams' 
+	},
+	{
+		id: 6,
+		exerciseId: 3,
+		comment: "Bench presses always make me feel pumped for the week. My ABSOLUTE favorite.",
+		username: 'Ken Smith'
+	},
+	{
+		id: 7,
+		exerciseId: 3,
+		comment: "I will definitely second what Ken said!!!",
+		username: 'Danny Williams'
+	},
+	{
+		id: 8,
+		exerciseId: 3,
+		comment: "Everybody loves a good bench press set!",
+		username: 'Ken Smith'
+	},
+	{
+		id: 9,
+		exerciseId: 3,
+		comment: "💪",
+		username: 'Danny Williams'
+	}
+]
