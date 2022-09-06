b5ac1724b86d3521ee8fe41e15bae7cb80ad4b67
Po Lin <po.ting.lin@gmail.com>
2022-09-06 12:45:12 -0400

    Add logic to check username availability

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -2,7 +2,19 @@ const knex = require('knex')(require('../knexfile').development);
 const uuid = require('uuid');
 
 exports.checkUsername = (req, res) => {
-	console.log('inside checkUsername in signupController');
+	const { username } = req.params;
+
+	knex('users')
+		.where({ username: username })
+		.then(data => {
+			if(data.length === 0){
+				res.status(200).json({ available: true });
+			}
+			res.status(200).json({ available: false });
+		})
+		.catch(err => {
+			res.status(500).send("An unexpected error occured :", err);
+		});
 };
 
 exports.checkPassword = (req, res) => {
